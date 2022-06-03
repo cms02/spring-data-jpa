bb50fb3e68a936115317806383227665b33368c0
cms02 <minoui1234@gmail.com>
2022-06-03 23:35:52 +0900

    반환 타입

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -12,6 +12,7 @@ import study.datajpa.entity.Team;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -153,6 +154,25 @@ class MemberRepositoryTest {
         for (Member member : result) {
             System.out.println("member = " + member);
         }
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findListByUsername("asdas");
+        System.out.println("result = " + result.size());
+
+        Member findMember = memberRepository.findMemberByUsername("asdas");
+        System.out.println("findMember = " + findMember);
+
+        Optional<Member> findMemberOptional = memberRepository.findOptionalByUsername("asdas");
+        System.out.println("findMemberOptional = " + findMemberOptional);
+
 
     }
+
 }
