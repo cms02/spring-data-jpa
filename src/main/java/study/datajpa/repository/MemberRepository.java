4be4d8d8fa28fa8c89e83b285d6847462a18b7a3
cms02 <minoui1234@gmail.com>
2022-06-08 16:54:44 +0900

    Projections

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -69,6 +69,10 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Member> findLockByUsername(String username);
 
+    List<UsernameOnly> findProjectionsByUsername(@Param("username") String username);
+
+    <T> List<T> findProjectionsByUsername(String username, Class<T> type);
+
 
 
 
